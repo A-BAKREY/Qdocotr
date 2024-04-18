617c424404364e740f05fa2c0c24003c885d191a
Ahmed_ElBakry <ahmedelbakrey123@gmail.com>
2024-04-18 15:00:35 +0200

    chatting

--- app/src/main/java/com/example/clinicapp/booking/adapter/BookingAdapter.kt
+++ app/src/main/java/com/example/clinicapp/booking/adapter/BookingAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.clinicapp.booking.adapter
 
-class BookingAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clinicapp.R
+import com.example.clinicapp.booking.model.BookingModel
+
+
+class BookingAdapter (private var dataSet: List<BookingModel>) :
+    RecyclerView.Adapter<BookingAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView1: TextView = view.findViewById(R.id.fromTime)
+        val textView2: TextView = view.findViewById(R.id.toTime)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.reservation_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BookingAdapter.MyViewHolder, position: Int) {
+        holder.textView1.text = dataSet[position].from
+        holder.textView2.text = dataSet[position].to
+
+    }
+
+    override fun getItemCount() = dataSet.size
 }
